f7d3366a7509624573d673a9fae05953c281e96c
Gonzalo Henriquez <gonzalomhenriquez86@gmail.com>
2025-10-10 16:53:25 -0300

    Se elimina usuarios del codigo

--- my_app/src/app.module.ts
+++ my_app/src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './modules/tasks/tasks.module';
-import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [TasksModule, UsersModule],
+  imports: [TasksModule],
 })
 export class AppModule {}
